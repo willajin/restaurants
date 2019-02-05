1cdaec5308489d7593fd74ecca18997ddb791051
ymsebast <ymsebast@uci.edu>
2019-02-04 18:47:53 -0800

    added server js

---
+++ server.js
@@ -0,0 +1,18 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Create a set of variables (hint: arrays of objects) for holding the reservation and waitlist data
+// Create a set of routes that then display this data as JSONs. Users should be given these JSONs if they visit the appropriate page (i.e. if a user visits localhost:3000/api/tables they should see a JSON of table data).
+// Frontend Team:
+// Temporarily join the backend team. Your task will be to create Express routes that display your HTML pages when a user visits the appropriate page. (i.e. if a user visits localhost:3000/tables... they should be shown the table.html page.)
+// If you finish early begin creating the code necessary to convert your form data into JSON objects.
